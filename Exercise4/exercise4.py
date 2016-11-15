fcd1d118244dfa680dc3470f79cd10ef5b08ad8a
themightyoarfish <rdiederichse@uos.de>
2016-11-15 20:13:46 +0100

    don't run stuff outside session

--- Exercise4/exercise4.py
+++ Exercise4/exercise4.py
@@ -1,23 +1,22 @@
 from utils import load_mnist_data, minibatches, one_hot
 import tensorflow as tf
 from progressbar import * # sudo pip3 install progressbar33
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 if __name__ == "__main__":
     d_train, l_train, d_test, l_test, d_val, l_val = load_mnist_data()
-    batch_size = 1
-    n_epochs = 10
+    batch_size = 100
+    n_epochs = 1000
 
     ############################################################################
     #                              Define network                              #
     ############################################################################
-    kernel1_size = 5
-    input_size = (28, 28)
-    n1_feature_maps = 32
     # Input layer
-    kernels1 = tf.Variable(tf.truncated_normal([kernel1_size,kernel1_size,1,n1_feature_maps], stddev=.1))
-    bias1    = tf.Variable(tf.constant(.1, shape=[n1_feature_maps]))
+    kernels1 = tf.Variable(tf.truncated_normal([5,5,1,32], stddev=.1))
+    bias1    = tf.Variable(tf.constant(.1, shape=[32]))
     x_flat   = tf.placeholder(tf.float32, shape=[None, 784])
-    x        = tf.reshape(x_flat, [-1, input_size[0], input_size[1], 1])
+    x        = tf.reshape(x_flat, [-1,28,28,1])
     d        = tf.placeholder(tf.int32, [None, 10])
 
     # Apply convolution kernels to input x
@@ -31,35 +30,28 @@ if __name__ == "__main__":
     kernel2_size    = 5
     n2_feature_maps = 64
     bias2           = tf.Variable(tf.constant(.1, shape=[n2_feature_maps]))
-    kernels2        = tf.Variable(tf.truncated_normal([kernel2_size, kernel2_size,
-        n1_feature_maps, n2_feature_maps], stddev=.1))
+    kernels2        = tf.Variable(tf.truncated_normal([5,5,32,64], stddev=.1))
     convolution2    = tf.nn.conv2d(pool1, kernels2, strides=[1,1,1,1], padding="SAME")
     conv2           = activation2 = tf.nn.tanh(convolution2 + bias2)
     pool2           = tf.nn.max_pool(activation2, ksize=[1,2,2,1],
             strides=[1,2,2,1], padding="SAME") # question: how do I select the kernel sizes for pooling or convolution?
 
     # first FF layer
-    n3_ffn = 1024
-    bias3  = tf.Variable(tf.constant(.1, shape=[n3_ffn]))
-    pool2_shape = pool2.get_shape()
-    ffn3_weights = tf.Variable(tf.truncated_normal([pool2_shape[1].value * pool2_shape[2].value
-        * pool2_shape[3].value, n3_ffn]))
-    activation3 = tf.nn.tanh(tf.matmul(tf.reshape(pool2, [batch_size,-1]), ffn3_weights) + bias3)
+    bias3  = tf.Variable(tf.constant(.1, shape=[1024]))
+    ffn3_weights = tf.Variable(tf.truncated_normal([7 * 7 * 64, 1024]))
+    activation3 = tf.nn.tanh(tf.matmul(tf.reshape(pool2, [-1, 7 * 7 * 64]), ffn3_weights) + bias3)
      
     # seconf FF layer (readout)
-    n4_ffn = 10
-    bias4  = tf.Variable(tf.constant(.1, shape=[n4_ffn]))
-    ffn3_shape = activation3.get_shape()
-    ffn4_weights = tf.Variable(tf.truncated_normal([ffn3_shape[1].value, n4_ffn]))
+    bias4  = tf.Variable(tf.constant(.1, shape=[10]))
+    ffn4_weights = tf.Variable(tf.truncated_normal([1024, 10]))
     y = activation4 = tf.matmul(activation3, ffn4_weights) + bias4
 
     ############################################################################
     #                         Define training process                          #
     ############################################################################
     
-    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(y, d)
-    optimizer     = tf.train.GradientDescentOptimizer(learning_rate=0.1)
-    training_step = optimizer.minimize(cross_entropy)
+    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, d))
+    training_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
     ############################################################################
     #                           Accuracy computation                           #
@@ -67,8 +59,8 @@ if __name__ == "__main__":
     
     # check if neuron firing strongest coincides with max value position in real
     # labels
-    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(d, 1))
-    accuracy           = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(d,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     
     ############################################################################
     #                              Train the net                               #
@@ -79,9 +71,14 @@ if __name__ == "__main__":
         widgets = ['Training: ', Percentage(), ' ', AnimatedMarker(markers='←↖↑↗→↘↓↙'), ' ', ETA()]
         pbar    = ProgressBar(widgets=widgets, maxval=n_epochs).start()
         for i in range(n_epochs):
-            for mb, labels in minibatches(d_train, l_train, batch_size=batch_size):
-                sess.run(training_step, feed_dict={x_flat: mb, d: labels})
-            current_accuracy = sess.run(accuracy, feed_dict={x_flat: d_train, d: one_hot(l_train, 10)})
-            print(current_accuracy)
+            # for mb, labels in minibatches(d_train, l_train, batch_size=batch_size):
+            #     sess.run(training_step, feed_dict={x_flat: mb, d: labels})
+            # current_accuracy = sess.run(accuracy, feed_dict={x_flat: d_train, d: one_hot(l_train, 10)})
+            # print(current_accuracy)
             pbar.update(i)
+            batch = mnist.train.next_batch(batch_size)
+            sess.run(training_step, feed_dict={x_flat: batch[0], d: batch[1]})
+            if (i + 1) % 10 == 0:
+                curr_acc = sess.run(accuracy, feed_dict={ x_flat: batch[0], d: batch[1]})
+                print("step %d, accuracy on current batch: %g" % (i, curr_acc))
 
