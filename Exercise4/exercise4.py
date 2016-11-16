51f8d36cd494536843a3a066ebe6b87103dec739
ktrebing@uni-osnabrueck.de <ktrebing@uni-osnabrueck.de>
2016-11-16 22:24:27 +0100

    added interactive plot

--- Exercise4/exercise4.py
+++ Exercise4/exercise4.py
@@ -2,6 +2,7 @@ from utils import load_mnist_data, minibatches, one_hot
 import tensorflow as tf
 from progressbar import * # sudo pip3 install progressbar33
 from tensorflow.examples.tutorials.mnist import input_data
+import matplotlib.pyplot as plt
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 if __name__ == "__main__":
@@ -41,7 +42,7 @@ if __name__ == "__main__":
     bias3        = tf.Variable(tf.constant(.1, shape=[1024]))
     ffn3_weights = tf.Variable(tf.truncated_normal([7 * 7 * 64, 1024]))
     activation3  = tf.nn.tanh(tf.matmul(tf.reshape(pool2, [-1, 7 * 7 * 64]), ffn3_weights) + bias3)
-     
+
     # second FF layer (readout)
     bias4        = tf.Variable(tf.constant(.1, shape=[10]))
     ffn4_weights = tf.Variable(tf.truncated_normal([1024, 10]))
@@ -50,7 +51,7 @@ if __name__ == "__main__":
     ############################################################################
     #                         Define training process                          #
     ############################################################################
-    
+
     cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, d))
     # I also tried the gradient descent optimizer, but that did not improve
     # performance withe learning rates 0.2, 0.5 or 0.8. Maybe one would hate to
@@ -60,20 +61,26 @@ if __name__ == "__main__":
     ############################################################################
     #                           Accuracy computation                           #
     ############################################################################
-    
+
     # check if neuron firing strongest coincides with max value position in real
     # labels
     correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(d,1))
     accuracy           = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    
+    training_step_accuracy = []
+
     ############################################################################
     #                              Train the net                               #
     ############################################################################
-    
+
     with tf.Session() as sess:
         sess.run(tf.initialize_all_variables())
         widgets = ['Training: ', Percentage(), ' ', AnimatedMarker(markers='←↖↑↗→↘↓↙'), ' ', ETA()]
         pbar    = ProgressBar(widgets=widgets, maxval=n_epochs).start()
+
+        plt.ion()
+        plt.gca().set_ylim([0,1])
+        plt.gca().set_xlim([0,n_epochs/50])
+
         for i in range(n_epochs):
             pbar.update(i)
             batch = mnist.train.next_batch(batch_size)
@@ -81,7 +88,17 @@ if __name__ == "__main__":
             if i > 1 and i % 50 == 0:
                 curr_acc = sess.run(accuracy, feed_dict={ x_flat: batch[0], d:
                     batch[1] })
-                print("step %d, accuracy on current batch: %g" % (i, curr_acc))
-        test_acc = sess.run(accuracy, feed_dict={ x_flat: mnist.test.images, d: mnist.test.labels })
-        print("Final accuracy on test set: %g" % test_acc)
+                training_step_accuracy.append(curr_acc)
+                plt.plot(training_step_accuracy, color = "b")
+                plt.draw()
+                plt.pause(0.0001)
 
+                # print("step %d, accuracy on current batch: %g" % (i, curr_acc))
+        # I split this up to not run out of memory
+        test_acc1 = sess.run(accuracy, feed_dict={ x_flat: mnist.test.images[:5000], d: mnist.test.labels[:5000] })
+        test_acc2 = sess.run(accuracy, feed_dict={ x_flat: mnist.test.images[5000:], d: mnist.test.labels[5000:] })
+        test_acc = (test_acc1+test_acc2)/2
+        print("Final accuracy on test set: %g" % test_acc)
+        plt.plot(training_step_accuracy, color = "b")
+        plt.ioff()
+        plt.show()
